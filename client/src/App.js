9681b8bebb75ede5dae557dedde57f1c9406e2a6
abbassaad27 <xenozb@gmail.com>
2021-09-08 23:58:15 +0600

    added upload to db functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Uploader from "./components/uploader/uploader.component";
 
 function App() {
   const [files, setFiles] = useState(null);
+
+  useEffect(() => {
+    if (files) {
+      const formData = new FormData();
+
+      formData.append("file-input", files);
+
+      fetch("/", {
+        method: "post",
+        body: formData,
+      }).catch(console.error);
+    }
+  }, [files]);
 
   return (
     <div className="App">{files ? null : <Uploader setFiles={setFiles} />}</div>
