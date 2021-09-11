efd24ed30392c7c3a0e3e917f14f8789b46347c8
Faiyaz Rahman <faiyazrahman03@gmail.com>
2021-09-11 11:01:06 +0600

    updated package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,51 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Loader from "./components/loader/loader.component";
-import Uploader from "./components/uploader/uploader.component";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Loader from './components/loader/loader.component';
+import Uploader from './components/uploader/uploader.component';
 
 function App() {
-  const [files, setFiles] = useState(null);
-  const [loader, setLoader] = useState(false);
+   const [files, setFiles] = useState(null);
+   const [loader, setLoader] = useState(false);
 
-  useEffect(() => {
-    if (files) {
-      setLoader(true);
-      const formData = new FormData();
+   useEffect(() => {
+      if (files) {
+         setLoader(true);
+         const formData = new FormData();
 
-      formData.append("file-input", files);
+         formData.append('file-input', files);
 
-      fetch("/", {
-        method: "post",
-        body: formData,
-      }).catch(console.error);
+         fetch('http://localhost:5000/', {
+            method: 'post',
+            body: formData,
+         }).catch(console.error);
 
-      setTimeout(() => {
-        setLoader(false);
-      }, 5000);
-    }
-  }, [files]);
+         setTimeout(() => {
+            setLoader(false);
+         }, 5000);
+      }
+   }, [files]);
 
-  if (!files) {
-    return (
-      <div className="App">
-        <Uploader setFiles={setFiles} />
-      </div>
-    );
-  }
-  if (loader) {
-    return (
-      <div className="App">
-        <Loader />
-      </div>
-    );
-  }
-  if (files && !loader) {
-    return (
-      <div className="App">
-        <Uploader imgSrc={files.name} />
-      </div>
-    );
-  }
+   if (!files) {
+      return (
+         <div className="App">
+            <Uploader setFiles={setFiles} />
+         </div>
+      );
+   }
+   if (loader) {
+      return (
+         <div className="App">
+            <Loader />
+         </div>
+      );
+   }
+   if (files && !loader) {
+      return (
+         <div className="App">
+            <Uploader imgSrc={files.name} />
+         </div>
+      );
+   }
 }
 
 export default App;
